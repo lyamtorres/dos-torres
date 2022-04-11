9ead60310b58f612f0d5f34cec5032c9e8d91b5e
lyamtorres <lyamtorres@hotmail.com>
2022-04-11 23:04:26 +0200

    enhance landing page

--- app/Views/site.php
+++ app/Views/site.php
@@ -16,18 +16,18 @@
 			</button>
 			<div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
 				<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Novedades <span class="sr-only"></span></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Quiénes somos</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Galería</a>
-				</li>
-				<li class="nav-item">
-				<a class="nav-link" href="#">Contacto</a>
-				</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Inicio<span class="sr-only"></span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Quiénes somos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Galería</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Contáctanos</a>
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -35,31 +35,13 @@
   </header>	
 	<body>
 		<section class="hero">
-			<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-				<div class="carousel-indicators">
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-					<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 0"></button>
+			<div class="container">
+				<div class="content d-flex align-items-center">
+					<h1>Dos Torres, Academia de Taekwon-Do ITF</h1>
+					<p>Escuela De Artes Marciales en Ciudad Sandino</p>
+					<p>Abrimos a las 17:00</p>
+					<button type="button" class="btn btn-primary btn-lg">Pedir una cotización</button>
 				</div>
-				<div class="carousel-inner">
-					<div class="carousel-item active">
-						<img src="<?=base_url('Images/carousel-one.jpeg')?>" class="d-block w-100" alt="...">
-					</div>
-					<div class="carousel-item">
-						<img src="<?=base_url('Images/carousel-two.jpeg')?>" class="d-block w-100" alt="...">
-					</div>
-					<div class="carousel-item">
-						<img src="<?=base_url('Images/carousel-three.jpg')?>" class="d-block w-100" alt="...">
-					</div>
-				</div>
-				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Previous</span>
-				</button>
-				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="visually-hidden">Next</span>
-				</button>
 			</div>
 	  	</section>
 		<section class="news">
@@ -130,6 +112,7 @@
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.3513185245156!2d-86.35415318518633!3d12.156468091394286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f7151e9a4f9bfa1%3A0xc93f207dd225359a!2sDos%20Torres%2C%20Academia%20de%20Taekwon-Do%20ITF!5e0!3m2!1ses-419!2sfr!4v1649002557557!5m2!1ses-419!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 			<div>
 	  	</section>
+		  
 	<footer>
 		<p>© Dos Torres 2022<br></p>
 	</footer>
